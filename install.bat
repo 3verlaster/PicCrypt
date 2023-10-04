737385a6d9619b6995c4290dbe08457c186faea8
3verlaster <96373998+3verlaster@users.noreply.github.com>
2023-10-04 19:34:25 +0300

    [WINDOWS]
    [+] install.bat

---
+++ install.bat
@@ -0,0 +1,10 @@
+@echo off
+REM  ======================================
+REM  This file is part of:
+REM  https://github.com/3verlaster/PicCrypt
+REM  ======================================
+@echo.
+@echo https://github.com/3verlaster/PicCrypt
+@echo.
+
+pip install pycryptodome
