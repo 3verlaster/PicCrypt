6da18645e7347b224b8c62454014cdb8d74c67cd
3verlaster <96373998+3verlaster@users.noreply.github.com>
2023-10-04 19:34:49 +0300

    [LINUX]
    [+] install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#  ======================================
+#  This file is part of:
+#  https://github.com/3verlaster/PicCrypt
+#  ======================================
+
+echo ""
+echo "https://github.com/3verlaster"
+echo ""
+
+pip install pycryptodome
